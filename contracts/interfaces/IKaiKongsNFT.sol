c103c13be77c43049618dbb2cecb0048c68fc9ac
BladeOfCode <bladeofcode@gmail.com>
2023-05-15 05:20:29 -0700

    fix/insert-interface

--- contracts/interfaces/IKaiKongsNFT.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
-
-interface IKaiKongsNFT {
-    function getRoyaltyFee() external view returns (uint256);
-
-    function getRoyaltyRecipient() external view returns (address);
-}
